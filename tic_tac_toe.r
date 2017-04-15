354fb1515baecafe7804d5209c2ff3737172c73f
88superman <kungleungli@gmail.com>
2017-04-15 12:37:24 +0800

    first commit. bug

--- tic_tac_toe.r
+++ tic_tac_toe.r
@@ -59,20 +59,45 @@ learning <- function(alpha = 0.1, random = 0.1,
 		
 			## Update experience--------------
 			x <- t(possible_move(current_state, turn = turn))
-			x_base3 <- apply(x + 1, 1, base3_to_decimal)
-			learned <- which_equal_C_2(learned_state[[1 + turn]][,11], x_base3)
+			identify_symmetry <- function(x){
+				x_symmetry <- NULL
+				for(i in 1:nrow(x)){
+					# Add symmetry index
+					x_equi <- cbind(equivalent_C(x[i, ]), symmetry = 1:8)
+					# Add state identifier 
+					x_equi_base3 <- apply(x_equi[, 1:9] + 1, 1, base3_to_decimal)
+					# Drop duplicated 
+					x_equi <- cbind(x_equi, x_equi_base3)[!duplicated(x_equi_base3),]
+					x_equi <- matrix(x_equi, ncol = 11)
+					learned <- which_equal_C_2(learned_state[[1 + turn]][,11], x_equi[,11])
+					if(sum(learned) == 0){
+						# if not learned, learn one of them
+						x_symmetry <- rbind(x_symmetry, x_equi[1, ])
+					} else {
+						# else, treat it as the equivalent one
+						x_symmetry <- rbind(x_symmetry, x_equi[which(learned != 0)[1], ])						
+					}
+				}
+				return(x_symmetry)
+			}
+			x_symmetry <- identify_symmetry(x)
+			
+			learned <- which_equal_C_2(learned_state[[1 + turn]][,11], x_symmetry[, 11])
 			
 			option <- learned[learned!=0]
+			transform_index <- rep(4, length(learned)) # 4: identity
 			# If not seen possible move, then assign it with 0.5
 			if(sum(learned == 0) > 0){
 				option <- c(option, nrow(learned_state[[1 + turn]]) + 1:sum(learned == 0))
-				new_state <- x[learned == 0, ]
+				new_state <- x_symmetry[learned == 0, ]
 				learned_state[[1 + turn]] <- rbind(learned_state[[1 + turn]], 
-												cbind(matrix(new_state, nrow=sum(learned == 0)), 
+												cbind(matrix(new_state[, 1:9], nrow=sum(learned == 0)), 
 													  0.5,
-													  apply(matrix(new_state + 1, ncol = 9), 1, base3_to_decimal)))
+													  new_state[, 11]))
+				transform_index <- c(rep(0, sum(learned != 0)), new_state[, 10])
+													  
 			}
-
+			
 			## Decision----------------------
 			decision_values <- learned_state[[1 + turn]][option, 10]
 			random_move <- runif(1) < random
@@ -82,12 +107,19 @@ learning <- function(alpha = 0.1, random = 0.1,
 				which_option <- option[sample.vec(which_equal_C(decision_values, max(decision_values)), 1)]
 			}
 			which_option_hist[[1 + turn]] <- c(which_option_hist[[1 + turn]], which_option)
+			
+			transform_index <- transform_index[option == which_option]
+			
 			decision <- learned_state[[1 + turn]][which_option, ]
 			last_move <- which_option_hist[[1 + turn]][length(which_option_hist[[1 + turn]]) - 1]
 			last_move_2 <- which_option_hist[[1 + turn]][length(which_option_hist[[1 + turn]]) - 2]
 
 			old_value <- learned_state[[1 + turn]][last_move, 10]
+			
+			# Reverse transformation
 			current_state <- decision[1:9]
+			current_state <- revert_symmetry_C(current_state, transform_index)
+			
 			current_status <- check_status(current_state, turn)
 			
 			## Learning---------------------
