6723a0c850ebecd1ae24d2b1f8fab79ef0f25279
tomli1234 <tomli123@hku.hk>
2017-04-24 12:17:39 +0800

    possible moves and game status

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,19 +6,43 @@
 
 import numpy as np
 import timeit
-import time
 
 # Identify possible moves-----------------------------------
-S = np.repeat(-1, 9, axis = 0)
 def possible_moves(state, turn):
-    move = [i for i, s in enumerate(S) if s == -1]
+    empty = [i for i, s in enumerate(S) if s == -1]
     S_next = []
-    for i in move:
+    for i in empty:
         s_next = state.tolist()
         s_next[i] = turn
         S_next.append(s_next)
     return S_next
-            
-possible_moves(S, 0)
-timeit.timeit(lambda: possible_moves(S, 0), number = 1)
 
+S = np.repeat(-1.0, 9, axis = 0)
+possible_moves(S, 0)
+timeit.timeit(lambda: possible_moves(S, 0), number = 1000)/1000
+
+# Check game status---------------------------------------
+def game_status(state, turn):
+    empty = [i for i, s in enumerate(S) if s == -1]
+    S[empty] = None
+    S_mat = S.reshape(3,3)
+    colsum = S_mat.sum(axis = 0)
+    rowsum = S_mat.sum(axis = 1)
+    diagsum_1 = sum(np.diag(S_mat))
+    diagsum_2 = sum(np.diag(np.fliplr(S_mat)))
+    allsum = np.append(diagsum_2, np.append(diagsum_1, np.append(rowsum, colsum)))
+    
+    if any(i == 3 for i in allsum):
+        return(turn)
+    elif any(i == 0 for i in allsum):
+        return(1- turn)
+    elif any(np.isnan(i) for i in allsum):
+        return(1 - turn)
+        
+S = np.array([1,1,1,0,0,1,-1,-1,-1], dtype = float)    
+timeit.timeit(lambda: game_status(S, 0), number = 1000)/1000
+
+
+  
+        
+        
