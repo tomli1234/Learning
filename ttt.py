a8ec091168282fba7e8fcb5cfd9c8758995daadf
88superman <kungleungli@gmail.com>
2018-09-08 11:42:23 +0800

    simulate update_Q

--- ttt.py
+++ ttt.py
@@ -13,35 +13,39 @@ class player():
         self.position = position
         initial_Q = np.zeros((3, 3))
         initial_Q[:] = np.nan
-        self.Q = pd.DataFrame([{'state': initial_Q, 'action': (0, 0), 'Q': 0.5}])
+        self.Q = pd.DataFrame([{'state': initial_Q, 'action': (0, 0), 'Q': 0}])
 
     
     def move(self, g):
-        valid = np.isnan(g.state)
-        i, j = np.where(valid)
-        self.state_t = g.state + 0 # why upadte automatically if not + 0?
+      
+        if g.finish == False:
+            valid =np.isnan(g.state)
+            i, j = np.where(valid)
+            self.state_t = g.state + 0 # why upadte automatically if not + 0?
+    
+            
+            # random move
+            selected = np.random.choice(len(i), 1)[0]
+    
+            self.move_t = i[selected], j[selected]
+    
+            # Change state
+            g.state[self.move_t] = self.position
 
-        
-        # random move
-        selected = np.random.choice(len(i), 1)[0]
+            # Check winner
+            g.check_winner()
+            self.win = g.winner
+            
+            # Check finish
+            g.check_finish()
 
-        self.move_t = i[selected], j[selected]
-
-        # Change state
-        g.state[self.move_t] = self.position
-
-        # Check winner
-        g.check_winner()
-        self.win = g.winner
-        
-        # Check finish
-        g.check_finish()
     
     def update_Q(self):
         reward = np.float(self.win[self.position])
         
         Q = reward
-        print([self.state_t, self.move_t, Q])
+        self.Q = self.Q.append(pd.DataFrame([{'state': self.state_t, 'action': self.move_t, 'Q': Q}]))
+        print(self.Q)
         
 
         
@@ -51,9 +55,11 @@ class game():
     def __init__(self):
         self.state = np.zeros((3, 3))
         self.state[:] = np.nan
+        self.finish = False
+        self.winner = [0, 0]
+
 
     def check_winner(self):
-        self.winner = [0, 0]
         for i in [0, 1]:
             i_win = self.state == i
             row_sum = np.sum(i_win, axis = 1)
@@ -61,23 +67,25 @@ class game():
             self.winner[i] = (3 in row_sum) or (3 in col_sum)
             
     def check_finish(self):
-        self.finish = False
         if sum(self.winner) > 0 or np.sum(np.isnan(self.state)) == 0:
             self.finish = True
         
     
 
-game_1 = game()
 player_1 = player(1)
 player_0 = player(0)
 
-player_1.move(game_1)
-player_0.move(game_1)
-player_1.update_Q()
+for i in range(100):
+    game_1 = game()
+    while game_1.finish == False:
+        player_1.move(game_1)
+        player_0.move(game_1)
+        player_1.update_Q()
 
+        
+        
 print(game_1.state)
 
 game_1.winner
 game_1.finish
-
 
