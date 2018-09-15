54b0288c1f531469206a34315b8f0c7ed7ac3a84
88superman <kungleungli@gmail.com>
2018-09-15 11:40:48 +0800

    allow taking occupied move

--- ttt.py
+++ ttt.py
@@ -16,34 +16,45 @@ class player():
         self.Q = pd.DataFrame([{'state': initial_Q, 'action': (0, 0), 'Q': 0}])
 
     
-    def move(self, g):
+    def move(self, g, random):
       
         if g.finish == False:
-            valid = g.state == -100
-            i, j = np.where(valid)
-            self.state_t = g.state + 0 # why upadte automatically if not + 0?
+#            valid = g.state == -100
+#            i, j = np.where(valid)
+            self.state_t = g.state + 0 # why update automatically if not + 0?
     
             
-            # random move
-            selected = np.random.choice(len(i), 1)[0]
+            if random == True:
+                # random move
+    #            selected = np.random.choice(len(i), 1)[0]    
+    #            self.move_t = i[selected], j[selected]
+                self.move_t = np.random.choice(range(3), 1)[0], np.random.choice(range(3), 1)[0]
+            else:
+                self.seen_state = self.Q['state'].apply(lambda x: np.array_equal(x, g.state))
+                self.select_move = np.argmax(self.Q[self.seen_state]['Q'])
+                self.move_t = self.Q.iloc[self.select_move]['action']
 
-            #np.array_equal(game_1.state, player_1.Q['state'].iloc[0])
-    
-            self.move_t = i[selected], j[selected]
-    
-            # Change state
-            g.state[self.move_t] = self.position
+            # check repeated move
+            self.repeated_move = g.state[self.move_t] != -100
 
-            # Check winner
-            g.check_winner()
-            self.win = g.winner[self.position]
+            if self.repeated_move == False:
+                # Change state
+                g.state[self.move_t] = self.position
+                            
+                # Check winner
+                g.check_winner()
+                self.win = g.winner[self.position]
             
-            # Check finish
-            g.check_finish()
+                # Check finish
+                g.check_finish()
+            else:
+                g.finish = True
 
     
     def update_Q(self):
         reward = np.float(self.win)
+        if self.repeated_move == True:
+            reward = -10
         
         Q = reward
         self.Q = self.Q.append(pd.DataFrame([{'state': self.state_t, 'action': self.move_t, 'Q': Q}]))
@@ -77,14 +88,16 @@ class game():
 player_1 = player(1)
 player_0 = player(0)
 
-for i in range(100):
+for i in range(50):
     game_1 = game()
     while game_1.finish == False:
-        player_1.move(game_1)
-        player_0.move(game_1)
+        player_1.move(game_1, random = True)
+        player_0.move(game_1, random = True)
         player_1.update_Q()
 
-        
+game_1.state
+player_1.Q
+    
 # Evaluation
 # random move baseline
 player_1 = player(1)
@@ -94,8 +107,8 @@ win_record = []
 for i in range(1000):
     game_1 = game()
     while game_1.finish == False:
-        player_1.move(game_1)
-        player_0.move(game_1)
+        player_1.move(game_1, random = False)
+        player_0.move(game_1, random = True)
     win_record.append(player_1.win)
 
 pd.DataFrame(win_record)[0].value_counts()
